87c8f2e5b269bb26c05fd31f99298eac14c90f64
Harsha Vardhan <dpharshavardhan.1636@gmail.com>
2025-09-29 07:05:59 +0000

    make the blinking animation little slow. it to fast and grabbing to much

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -99,7 +99,7 @@ export default {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        'blink-alert': 'blink-alert 1.5s ease-in-out infinite'
+        'blink-alert': 'blink-alert 2.5s ease-in-out infinite'
       },
     },
   },
